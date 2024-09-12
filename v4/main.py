d7b099e52ef02b001f1acefa7c467d24bc44d832
miniPCB <nolan@minipcb.com>
2024-09-11 19:20:57 -0500

    Approaching multiple window bug

--- v4/main.py
+++ v4/main.py
@@ -1,12 +1,10 @@
-from setup import setup_tabs, initialize_application
-from labels import *
-from red_tags import *
+from tkinter import Tk
+from setup import SetupWindow
+
+def main():
+    root = Tk()
+    app = SetupWindow(root)
+    root.mainloop()
 
 if __name__ == "__main__":
-    current_board_name = current_board_rev = current_board_var = current_board_sn = None
-    root = tk.Tk()
-    
-    initialize_application()
-    setup_tabs(root)
-    
-    root.mainloop()
+    main()
